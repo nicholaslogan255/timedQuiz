3b6665e318c753e752a8f73b492ceec337a4bac5
Nic_Logan <nicholaslogan255@gmail.com>
2018-06-21 23:35:30 -0500

    Quiz functional

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -9,28 +9,119 @@ window.onload = function () {
 
     // time remaining is seconds
     var timeRemain = 90;
-    $("#time").text(timeConvert(timeRemain)); // show time
+    $("#time").text(timeConvert(timeRemain)); // show time on page load
+
+
+    var selectedAnswer = $("obsidian").val();
+
+
+    // when the submit button was clicked
+    $("#submit").on("click", function () {
 
 
 
+        // logs the selected value to the first question
+        console.log($("input:radio[name=obsidian]:checked").val());
+
+        // logs the selected value to the second question
+        console.log( $("input:radio[name=quartz]:checked").val() )
+
+
+       stopTime();
+
+        //todo: lock out quiz from changing answers
+        gradeQuiz();
+
+        $("submit").hide(); // hide submit button
+
+
+
+    });
+
+
+    function gradeQuiz() {
+        var resultsMessage = ""
+
+        var correct = 0;
+
+
+        if($("input:radio[name=obsidian]:checked").val() != "Igneous"){
+            resultsMessage +="Obsidian is an igneous rock! ";
+            
+        }
+        else{correct++;}
+
+        if($("input:radio[name=quartz]:checked").val() != "Sedimentary"){
+            resultsMessage +="Quartz is an Sedimentary rock! ";
+           
+        }
+        else{correct++;}
+
+        if($("input:radio[name=basalt]:checked").val() != "Igneous"){
+            resultsMessage +="basalt is an igneous rock! ";
+           
+        }else{correct++;}
+
+        if($("input:radio[name=marble]:checked").val() != "Metamorphic"){
+            resultsMessage +="marble is an Metamorphic rock! ";
+            
+        }else{correct++;}
+
+        if($("input:radio[name=sandstone]:checked").val() != "Sedimentary"){
+            resultsMessage +="sandstone is an Sedimentary rock! ";
+            
+        }else{correct++;}
+
+        if($("input:radio[name=schist]:checked").val() != "Metamorphic"){
+            resultsMessage +="schist is an Metamorphic rock! ";
+            
+        }else{correct++;}
+
+        resultsMessage += "You got "+correct+"/6 correct! "
+
+        if(correct == 6){
+            resultsMessage += "Congradulations. You got them all right!"
+        }
+
+
+
+        $("#results").append(resultsMessage);
+
+    }
+
+
+
+
+
+    // code that makes the countdown work
     function countDown() {
 
-        if(timeRemain > 1){
+        if (timeRemain > 1) { // count down if there is more time.
             timeRemain -= 1;
-            console.log("Time: "+timeRemain);
+            console.log("Time: " + timeRemain);
         }
-        else{
-            timeRemain -= 1;
-             clearInterval(intervalId);
-             console.log("#### Time's Up: "+timeRemain);
+        else { // when the time runs out...
+            
+            stopTime(); // stop the timer
+
+            //todo: lock out quiz from changing answers
+            gradeQuiz(); // show results
+
+            $("submit").hide(); // hide submit button
         }
-    
-        //  Get the current time, pass that into the timeConverter function,
+
         //   show the converted time in the "time" div.
         $("#time").text(timeConvert(timeRemain));
-      }
+    }
 
+    // stop timer
+    function stopTime(){
+        timeRemain -= 1;
+            clearInterval(intervalId); // stop timer
+            console.log("#### Time's Up: " + timeRemain); // log time up
+            $("#countdown").hide(); // hide the timer
 
+    }
 
 
     function timeConvert(inTime) {
@@ -38,20 +129,19 @@ window.onload = function () {
         //  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
         var minutes = Math.floor(inTime / 60);
         var seconds = inTime - (minutes * 60);
-    
+
         if (seconds < 10) {
-          seconds = "0" + seconds;
+            seconds = "0" + seconds;
         }
-    
+
         if (minutes === 0) {
-          minutes = "00";
+            minutes = "00";
         }
-    
         else if (minutes < 10) {
-          minutes = "0" + minutes;
+            minutes = "0" + minutes;
         }
-    
+
         return minutes + ":" + seconds;
-      }
+    }
 
 }
