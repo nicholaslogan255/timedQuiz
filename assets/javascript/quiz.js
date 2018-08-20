358f120cf720bccfc3c76513190e56b8d0eeceba
Nic_Logan <nicholaslogan255@gmail.com>
2018-08-20 11:59:55 -0500

    Added photos, fixed countdown location, fixed formatting of quiz results

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -19,15 +19,14 @@ window.onload = function () {
     $("#submit").on("click", function () {
 
 
-
         // logs the selected value to the first question
         console.log($("input:radio[name=obsidian]:checked").val());
 
         // logs the selected value to the second question
-        console.log( $("input:radio[name=quartz]:checked").val() )
+        console.log($("input:radio[name=quartz]:checked").val())
 
 
-       stopTime();
+        stopTime();
 
         //todo: lock out quiz from changing answers
         gradeQuiz();
@@ -44,46 +43,83 @@ window.onload = function () {
 
         var correct = 0;
 
-
-        if($("input:radio[name=obsidian]:checked").val() != "Igneous"){
-            resultsMessage +="Obsidian is an igneous rock! ";
-            
+        // q1
+        if ($("input:radio[name=obsidian]:checked").val() != "Igneous") {
+            let incorrect = "<p><span class = 'incorrect'>Incorrect.</span>Obsidian is an Igneous rock!</p>";
+            $("#results").append(incorrect);
         }
-        else{correct++;}
+        else {
 
-        if($("input:radio[name=quartz]:checked").val() != "Sedimentary"){
-            resultsMessage +="Quartz is an Sedimentary rock! ";
-           
+            let correct = "<p><span class = 'correct'>Correct!</span> Obsidian is an Igneous rock!</p>";
+            $("#results").append(correct);
+
+            correct++;
         }
-        else{correct++;}
 
-        if($("input:radio[name=basalt]:checked").val() != "Igneous"){
-            resultsMessage +="basalt is an igneous rock! ";
-           
-        }else{correct++;}
+        // q2
+        if ($("input:radio[name=quartz]:checked").val() != "Sedimentary") {
+            let incorrect = "<p><span class = 'incorrect'>Incorrect.</span> Quartz is an Sedimentary rock!</p>";
+            $("#results").append(incorrect);
+        }
+        else {
+            let correct = "<p><span class = 'correct'>Correct!</span> Quartz is an Sedimentary rock!</p>";
+            $("#results").append(correct);
 
-        if($("input:radio[name=marble]:checked").val() != "Metamorphic"){
-            resultsMessage +="marble is an Metamorphic rock! ";
-            
-        }else{correct++;}
-
-        if($("input:radio[name=sandstone]:checked").val() != "Sedimentary"){
-            resultsMessage +="sandstone is an Sedimentary rock! ";
-            
-        }else{correct++;}
-
-        if($("input:radio[name=schist]:checked").val() != "Metamorphic"){
-            resultsMessage +="schist is an Metamorphic rock! ";
-            
-        }else{correct++;}
-
-        resultsMessage += "You got "+correct+"/6 correct! "
-
-        if(correct == 6){
-            resultsMessage += "Congradulations. You got them all right!"
+            correct++;
         }
 
 
+        // q3
+        if ($("input:radio[name=basalt]:checked").val() != "Igneous") {
+            let incorrect = "<p><span class = 'incorrect'>Incorrect.</span> Basalt is an Igneous rock!</p>";
+            $("#results").append(incorrect);
+
+        } else {
+            let correct = "<p><span class = 'correct'>Correct!</span> Basalt is an Igneous rock!</p>";
+            $("#results").append(correct);
+            correct++;
+        }
+
+
+        // q4
+        if ($("input:radio[name=marble]:checked").val() != "Metamorphic") {
+            let incorrect = "<p><span class = 'incorrect'>Incorrect.</span> Marble is an Metamorphic rock!</p>";
+            $("#results").append(incorrect);
+
+        } else {
+            let correct = "<p><span class = 'correct'>Correct!</span> Marble is an Metamorphic rock!</p>";
+            $("#results").append(correct);
+            correct++;
+        }
+
+        // q5
+        if ($("input:radio[name=sandstone]:checked").val() != "Sedimentary") {
+            let incorrect = "<p><span class = 'incorrect'>Incorrect.</span> Sandstone is an Sedimentary rock!</p>";
+            $("#results").append(incorrect);
+
+        } else {
+            let correct = "<p><span class = 'correct'>Correct!</span> Sandstone is an Sedimentary rock!</p>";
+            $("#results").append(correct);
+            correct++;
+        }
+
+        // q6
+        if ($("input:radio[name=schist]:checked").val() != "Metamorphic") {
+            let incorrect = "<p><span class = 'incorrect'>Incorrect.</span> Schist is an Metamorphic rock!</p>";
+            $("#results").append(incorrect);
+
+        } else {
+            let correct = "<p><span class = 'correct'>Correct!</span> Schist is an Metamorphic rock!</p>";
+            $("#results").append(correct);
+            correct++;
+        }
+
+        resultsMessage += "You got " + correct + "/6 correct! "
+
+        if (correct == 6) {
+            resultsMessage += "Congratulations. You got them all right!"
+        }
+        else { resultsMessage += "Looks like you need to study up on your rocks." }
 
         $("#results").append(resultsMessage);
 
@@ -101,13 +137,13 @@ window.onload = function () {
             console.log("Time: " + timeRemain);
         }
         else { // when the time runs out...
-            
+
             stopTime(); // stop the timer
 
             //todo: lock out quiz from changing answers
             gradeQuiz(); // show results
 
-            
+
         }
 
         //   show the converted time in the "time" div.
@@ -115,14 +151,14 @@ window.onload = function () {
     }
 
     // stop timer and prevent questions from being changed
-    function stopTime(){
+    function stopTime() {
         timeRemain -= 1;
-            clearInterval(intervalId); // stop timer
-            console.log("#### Time's Up: " + timeRemain); // log that time up
+        clearInterval(intervalId); // stop timer
+        console.log("#### Time's Up: " + timeRemain); // log that time up
 
-            $("#submit").hide(); // hide submit button
-            $("#countdown").hide(); // hide the timer
-            $("#quizForm").hide();
+        $("#submit").hide(); // hide submit button
+        $("#countdown").hide(); // hide the timer
+        $("#quizForm").hide();
 
 
 
